88ceea0b97af9ff662b81ef8449d2143cc41cbe6
Kiki <kirstinlisnoff5@gmail.com>
2025-02-19 22:57:01 -0500

    worked on truck.ts, tow method

--- Develop copy/src/classes/Cli.ts
+++ Develop copy/src/classes/Cli.ts
@@ -10,12 +10,12 @@ class Cli {
   // TODO: update the vehicles property to accept Truck and Motorbike objects as well
   // TODO: You will need to use the Union operator to define additional types for the array
   // TODO: See the AbleToTow interface for an example of how to use the Union operator
-  vehicles: (Car)[];
+  vehicles: (Car | Truck | Motorbike)[];
   selectedVehicleVin: string | undefined;
   exit: boolean = false;
 
   // TODO: Update the constructor to accept Truck and Motorbike objects as well
-  constructor(vehicles: (Car)[]) {
+  constructor(vehicles: (Car | Truck | Motorbike)[]) {
     this.vehicles = vehicles;
   }
 
@@ -61,7 +61,7 @@ class Cli {
           name: 'vehicleType',
           message: 'Select a vehicle type',
           // TODO: Update the choices array to include Truck and Motorbike
-          choices: ['Car'],
+          choices: ['Car', 'Truck', 'Motorbike'],
         },
       ])
       .then((answers) => {
@@ -70,6 +70,12 @@ class Cli {
           this.createCar();
         }
         // TODO: add statements to create a truck or motorbike if the user selects the respective vehicle type
+        else if (answers.vehicleType === 'Truck') {
+          this.createTruck();
+        } 
+        else if (answers.vehicleType === 'Motorbike') {
+          this.createMotorbike();
+        }
       });
   }
 
@@ -170,12 +176,26 @@ class Cli {
         },
       ])
       .then((answers) => {
-        // TODO: Use the answers object to pass the required properties to the Truck constructor
-        // TODO: push the truck to the vehicles array
-        // TODO: set the selectedVehicleVin to the vin of the truck
-        // TODO: perform actions on the truck
+        const truck = new Truck(
+          // TODO: The generateVin method is static and should be called using the class name Cli, make sure to use Cli.generateVin() for creating a truck and motorbike as well!
+          Cli.generateVin(),
+          answers.color,
+          answers.make,
+          answers.model,
+          parseInt(answers.year),
+          parseInt(answers.weight),
+          parseInt(answers.topSpeed),
+          parseInt(answers.towingCapacity),
+        );
+        // push the truck to the vehicles array
+        this.vehicles.push(truck);
+        // set the selectedVehicleVin to the vin of the truck
+        this.selectedVehicleVin = truck.vin;
+        // perform actions on the truck
+        this.performActions();
       });
   }
+    
 
   // method to create a motorbike
   createMotorbike(): void {
--- Develop copy/src/classes/Truck.ts
+++ Develop copy/src/classes/Truck.ts
@@ -6,22 +6,65 @@ import Wheel from './Wheel.js';
 import AbleToTow from '../interfaces/AbleToTow.js';
 
 // TODO: The Truck class should extend the Vehicle class and should implement the AbleToTow interface
-class Truck {
+class Truck extends Vehicle{
   // TODO: Declare properties of the Truck class
   // TODO: The properties should include vin, color, make, model, year, weight, top speed, wheels, and towing capacity
   // TODO: The types should be as follows: vin (string), color (string), make (string), model (string), year (number), weight (number), topSpeed (number), wheels (Wheel[]), towingCapacity (number)
+  vin: string;
+  color: string;
+  make: string;
+  model: string;
+  year: number;
+  weight: number;
+  topSpeed: number;
+  wheels: Wheel[];
+  towingCapacity: number;
 
   // TODO: Create a constructor that accepts the properties of the Truck class
+  constructor (
+    vin: string,
+    color: string,
+    make: string,
+    model: string,
+    year: number,
+    weight: number,
+    topSpeed: number,
+    wheels: Wheel[],
+    towingCapacity: number
+  ) {
     // TODO: The constructor should call the constructor of the parent class, Vehicle
+    super();
     // TODO: The constructor should initialize the properties of the Truck class
+    this.vin = vin;
+    this.color = color;
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.weight = weight;
+    this.topSpeed = topSpeed;
+    this.towingCapacity = towingCapacity;
     // TODO: The constructor should check if the wheels array has 4 elements and create 4 new default Wheel objects if it does not
+    if (wheels.length !== 4) {
+      this.wheels = [new Wheel(), new Wheel(), new Wheel(), new Wheel()]; 
+    } else {
+      this.wheels = wheels;
+    }
+  }
 
   // TODO: Implement the tow method from the AbleToTow interface
   tow(vehicle: Truck | Motorbike | Car): void {
     // TODO: Get the make an model of the vehicle if it exists
+    const vehicleMake = vehicle.make;
+    const vehicleModel = vehicle.model;
     // TODO: Check if the vehicle's weight is less than or equal to the truck's towing capacity
+    const vehicleWeight = vehicle.weight;
+    if (vehicleWeight <== this.towingCapacity) {
     // TODO: If it is, log that the vehicle is being towed
+    console.log(`${vehicleMake} ${vehicleModel} is being towed.`)
     // TODO: If it is not, log that the vehicle is too heavy to be towed
+    } else {
+      console.log(`${vehicleMake} ${vehicleModel} is too heavy to be towed.`);
+    }
   }
 
   // TODO: Override the printDetails method from the Vehicle class

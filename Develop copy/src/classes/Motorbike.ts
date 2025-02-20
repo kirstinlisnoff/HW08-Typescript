57bac491819722762d405fbe53e10dee0b9456d1
Kiki <kirstinlisnoff5@gmail.com>
2025-02-19 23:17:13 -0500

    added override printDetails method on Trucks.ts and Motorbikes.ts

--- Develop copy/src/classes/Motorbike.ts
+++ Develop copy/src/classes/Motorbike.ts
@@ -50,12 +50,30 @@ class Motorbike extends Vehicle{
   // TODO: Implement the wheelie method
   wheelie(vehicle: Motorbike):  void {
     // TODO: The method should log the message "Motorbike [make] [model] is doing a wheelie!"
+    console.log(`Motorbike ${this.make} ${this.model} is doing a wheelie!`);
   }
 
   // TODO: Override the printDetails method from the Vehicle class
+  override printDetails(): void {
+    
   // TODO: The method should call the printDetails method of the parent class
+  super.printDetails();
   // TODO: The method should log the details of the Motorbike
+  console.log(`VIN: ${this.vin}`);
+    console.log(`Color: ${this.color}`);
+    console.log(`Make: ${this.make}`);
+    console.log(`Model: ${this.model}`);
+    console.log(`Year: ${this.year}`);
+    console.log(`Weight: ${this.weight} lbs`);
+    console.log(`Top Speed: ${this.topSpeed} mph`);
   // TODO: The details should include the VIN, make, model, year, weight, top speed, color, and wheels
+  console.log(
+    `Wheel 1: ${this.wheels[0].getDiameter} inch with a ${this.wheels[0].getTireBrand} tire`
+  );
+  console.log(
+    `Wheel 2: ${this.wheels[1].getDiameter} inch with a ${this.wheels[1].getTireBrand} tire`
+  );
+  }
 }
 
 // Export the Motorbike class as the default export
